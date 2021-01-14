4845a23bfa5b1b8375b683a9cc7fc924eb5cc1d5
LuizEduardoBP <lepessoa38@gmail.com>
2021-01-13 21:02:26 -0300

    Alguns ajustes

--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -7,16 +7,16 @@ import { Header } from "../../components/header";
 
 export function Homepage() {
   const [character, setCharacter] = useState({ character: [] });
-  const [searchedData, setSearchedData] = useState([]) 
+  const [searchedData, setSearchedData] = useState([]);
   const [search, setSearch] = useState("");
-  const [load, setLoad] = useState(50)
+  // const [load, setLoad] = useState(50);
 
   useEffect(() => {
     async function Request() {
       try {
         const key =
           "?ts=1&apikey=af95085f5056fbb00e65e3bc94a455f2&hash=dca3d9faf47973b9c730b8e14b587db9";
-        const { data } = await api.get(`/v1/public/characters${key}&limit=${load}`);
+        const { data } = await api.get(`/v1/public/characters${key}&limit=50`);
         const characters = data.data.results;
         setCharacter({ character: characters });
       } catch (error) {
@@ -24,32 +24,40 @@ export function Homepage() {
       }
     }
     Request();
-  }, [load]);
+  }, []);
 
-  useEffect(()=>{
-    async function Search(){
-        try{
-          const key =
+  useEffect(() => {
+    async function Search() {
+      try {
+        const key =
           "?ts=1&apikey=af95085f5056fbb00e65e3bc94a455f2&hash=dca3d9faf47973b9c730b8e14b587db9";
-          const { data } = search!=="" ? await api.get(`/v1/public/characters${key}&nameStartsWith=${search.replace(" ", "%20")}`) : "";
-          data!==undefined ? setSearchedData(data.data.results) : setSearchedData([])
-        }catch(error){
-            console.error("Erro ao buscar itens", error)
-        }
+        const { data } =
+          search !== ""
+            ? await api.get(
+                `/v1/public/characters${key}&nameStartsWith=${search.replace(
+                  " ",
+                  "%20"
+                )}`
+              )
+            : "";
+        data !== undefined
+          ? setSearchedData(data.data.results)
+          : setSearchedData([]);
+      } catch (error) {
+        console.error("Erro ao buscar itens", error);
+      }
     }
-    Search()
-},[search])
+    Search();
+  }, [search]);
 
-console.log(searchedData)
+  console.log(searchedData);
 
-  
-
-  const data = search==="" ? character.character : searchedData
+  const data = search === "" ? character.character : searchedData;
 
   return (
     <>
-      <Header search={search} onChange={(e) => setSearch(e.target.value)}/>
-      <CharacterCard data={data}/>
+      <Header search={search} onChange={(e) => setSearch(e.target.value)} />
+      <CharacterCard data={data} />
     </>
   );
 }
